f3a1ba561a6190f725a02525b536690a1f8002d4
myantstew <myantstew@gmail.com>
2019-04-11 03:05:13 -0500

    Created test scene.

---
+++ Assets/scripts/scr_roadScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_roadScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject segment;
+    float zPosition;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        zPosition = transform.position.z;
+
+        transform.Translate(0,0,-1);
+        if(zPosition == -32)
+        {
+            CreateNewSegment();    
+        }
+        if(zPosition < -64)
+            Destroy(this.gameObject);
+
+    }
+    void CreateNewSegment()
+    {
+        Instantiate(segment, new Vector3(0,0,128),Quaternion.identity);
+    }
+
+}
---
+++ Assets/scripts/scr_testPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_testPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    float xmove;
+    Rigidbody myRigidbody;
+    Vector3 movement;
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        xmove = Input.GetAxis("Horizontal")*4;
+        movement = new Vector3(xmove,0,0);
+        myRigidbody.velocity = movement;
+
+    }
+}
