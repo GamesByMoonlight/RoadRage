5ddc10b770cb7678de08ab1f173d2ac8d4d632f4
ciscom <ciscom1971@yahoo.com>
2019-04-10 22:48:57 -0500

    Test AI car

--- Assets/scripts/scr_player.cs
+++ Assets/scripts/scr_player.cs
@@ -7,14 +7,25 @@ public class scr_player : MonoBehaviour
     public float xmove;
     public float zmove;
     Rigidbody myRigidbody;
+    Transform myTransform;
+    GameObject Car;
+
     void Start()
     {
+
+
+        Car = GameObject.Find("Cube");
         myRigidbody = GetComponent<Rigidbody>();
+        myTransform = GetComponent<Transform>();
     }
     void Update()
     {
+
         xmove = Input.GetAxis("Horizontal")*30;
         zmove = Input.GetAxis("Vertical")*30;
+
         myRigidbody.velocity = new Vector3(zmove, 0, xmove);
+       // Car.transform.rotation = Quaternion.Euler(0, xmove, 0);
+
     }
 }
