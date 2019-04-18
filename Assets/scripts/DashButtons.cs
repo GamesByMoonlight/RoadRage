2212b21659fd4508cee23f82542be3a9e4c6d594
ciscom <ciscom1971@yahoo.com>
2019-04-17 22:45:06 -0500

    Merge branch 'master' into feature/2
    # Conflicts:
    #	Assets/SampleScenes.meta
    #	Assets/SampleScenes/Materials.meta
    #	Assets/SampleScenes/Menu.meta
    #	Assets/SampleScenes/Models/Materials.meta
    #	Assets/Scenes/SampleScene.unity
    #	Assets/animations.meta
    #	Assets/audio.meta
    #	Assets/documents.meta
    #	Assets/materials/Materials/button 1.mat.meta
    #	Assets/materials/Materials/button 2.mat.meta
    #	Assets/prefabs/CarRig.prefab.meta

---
+++ Assets/scripts/DashButtons.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DashButtons : MonoBehaviour
+{
+
+
+    public float delayTime = 2f;
+    public string buttonType;
+
+    public void ActionButton(string ButtonType)
+    {
+        Invoke(ButtonType,0f);
+    }
+
+
+    void FireRocket()
+    {
+
+        Debug.Log("FireRocket");
+
+    }
+
+    void FireGun()
+    {
+        Debug.Log("FireGun");
+    }
+
+    void ReleaseOil()
+    {
+        Debug.Log("ReleaseOil");
+    }
+
+
+}
